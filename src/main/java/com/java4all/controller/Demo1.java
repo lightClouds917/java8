234b0770af9ee555a65015956fe54357ae60a159
ITyunqing <you@example.com>
2019-05-22 09:49:14 +0800

    java8 集合交集并集差集

--- src/main/java/com/java4all/controller/Demo1.java
+++ src/main/java/com/java4all/controller/Demo1.java
@@ -5,7 +5,7 @@ import java.util.List;
 import java.util.stream.Collectors;
 
 /**
- * @author wangzhongxiang
+ * @author IT云清
  * @date 2019年05月21日 20:06:49
  */
 public class Demo1 {
@@ -18,16 +18,39 @@ public class Demo1 {
         list1.add("5");
         list1.add("6");
         list1.add("7");
+        list1.add("8");
 
         List<String> list2 = new ArrayList<String>();
         list2.add("2");
-        list2.add("3");
         list2.add("7");
-        list2.add("8");
+        list2.add("10");
 
         //交集
         List<String> collect1 = list1.stream().filter(num -> list2.contains(num))
                 .collect(Collectors.toList());
+        System.out.println("交集");
         collect1.stream().forEach(System.out::println);
+
+        //差集 list1-list2
+        List<String> collect2 = list1.stream().filter(num -> !list2.contains(num))
+                .collect(Collectors.toList());
+        System.out.println("差集list1-list2");
+        collect2.stream().forEach(System.out::println);
+
+        //差集list2-list1
+        List<String> collect3 = list2.stream().filter(num -> !list1.contains(num))
+                .collect(Collectors.toList());
+        System.out.println("差集list2-list1");
+        collect3.stream().forEach(System.out::println);
+
+        //并集  不去重
+        list1.addAll(list2);
+        System.out.println("并集  不去重");
+        list1.stream().forEach(System.out::println);
+
+        //并集  去重
+        List<String> collect4 = list1.stream().distinct().collect(Collectors.toList());
+        System.out.println("并集  去重");
+        collect4.stream().forEach(System.out::println);
     }
 }
