8695c655dba0d21c4257f7f90c37e77bf5fc2961
ITyunqing <1186355422@qq.com>
2019-11-04 18:19:50 +0800

    add consumer

---
+++ src/test/java/com/java4all/ConsumerTest.java
@@ -0,0 +1,21 @@
+package com.java4all;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+/**
+ * @author ITyunqing
+ */
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ConsumerTest {
+
+    @Test
+    public void test(){
+
+    }
+
+
+}
