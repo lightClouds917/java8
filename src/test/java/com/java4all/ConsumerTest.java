7d15de14dfcff94757dfdc2629c757c8ed2c71f5
ITyunqing <1186355422@qq.com>
2019-11-05 22:19:00 +0800

    add consumer

--- src/test/java/com/java4all/ConsumerTest.java
+++ src/test/java/com/java4all/ConsumerTest.java
@@ -1,5 +1,8 @@
 package com.java4all;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.function.Consumer;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -14,8 +17,19 @@ public class ConsumerTest {
 
     @Test
     public void test(){
+        List<Integer> list1 = new ArrayList<>();
+        list1.add(1);
+        list1.add(3);
+        list1.add(7);
+        list1.add(19);
+        list1.add(20);
+
+        foreach(list1,(Integer i)->System.out.println(i));
 
     }
 
+    public static <T> void foreach(List<T> list,Consumer<T> consumer){
+        list.stream().forEach(obj->consumer.accept(obj));
+    }
 
 }
