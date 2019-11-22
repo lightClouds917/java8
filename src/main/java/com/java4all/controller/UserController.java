72de79f5b6ce34ef8d08bf7a3d6d1b3dcc472066
ITyunqing <1186355422@qq.com>
2019-11-22 08:54:56 +0800

    add test

--- src/main/java/com/java4all/controller/UserController.java
+++ src/main/java/com/java4all/controller/UserController.java
@@ -2,6 +2,8 @@ package com.java4all.controller;
 
 import com.java4all.entity.User;
 import java.util.List;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -14,6 +16,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping(value = "user")
 public class UserController {
 
+    private static final ExecutorService executorService = Executors.newFixedThreadPool(5);
     /**
      * web启动测试
      * @return
@@ -25,11 +28,15 @@ public class UserController {
 
 
     public static void main(String[]args){
+        for(int i = 0 ;i < 1000; i ++){
+            Runnable runnable = () -> test1();
+            executorService.submit(runnable);
+        }
 
     }
 
     public static void test1(){
-        List<User> users = User.initUser();
-
+        System.out.println(Thread.currentThread().getName());
+        int i = 4/0;
     }
 }
