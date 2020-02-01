dacda5c0caa4ef7030b49ccb24e7e1648c080bc4
ITyunqing <1186355422@qq.com>
2020-02-01 17:57:36 +0800

    add:test deepth

--- src/test/java/com/java4all/FunctionTest.java
+++ src/test/java/com/java4all/FunctionTest.java
@@ -1,10 +1,13 @@
 package com.java4all;
 
 
+import com.java4all.entity.Company;
+import com.java4all.entity.User;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 import java.util.function.Function;
+import javax.jws.soap.SOAPBinding.Use;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -32,4 +35,20 @@ public class FunctionTest {
     }
 
 
+    @Test
+    public void teset(){
+
+System.out.println(this.aa());
+
+    }
+
+    public boolean aa(){
+        B b = new B();
+        b = null;
+        return b == null || !(b instanceof A);
+    }
+
+
+class A{}
+class B extends A{}
 }
