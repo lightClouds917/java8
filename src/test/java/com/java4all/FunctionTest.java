a07ec015ead5a77c318c474bdead0f181fad8458
ITyunqing <1186355422@qq.com>
2020-02-01 17:59:45 +0800

    del:no use import

--- src/test/java/com/java4all/FunctionTest.java
+++ src/test/java/com/java4all/FunctionTest.java
@@ -1,13 +1,9 @@
 package com.java4all;
 
-
-import com.java4all.entity.Company;
-import com.java4all.entity.User;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 import java.util.function.Function;
-import javax.jws.soap.SOAPBinding.Use;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
